1f0ff7c0f67b09dac81903c14c67da5e62df4070
Austin Liu <arl96@cornell.edu>
2015-09-19 12:53:29 -0400

    finising cornell places

--- public/routes/cornell.js
+++ public/routes/cornell.js
@@ -10,7 +10,12 @@ router.get('/', function(req, res, next) {
     list.push({'Name': "Cornell Store", 'Lat': 42.446711, 'Lon': -76.484674});
     list.push({'Name': "Olin Library", 'Lat': 42.447914, 'Lon': -76.484324});
     list.push({'Name': "Bailey Hall", 'Lat': 42.449033, 'Lon': -76.480128});
-
+    list.push({'Name': "Schoellkopf Field", 'Lat': 42.443926, 'Lon': -76.478546});
+    list.push({'Name': "Risley Hall", 'Lat': 42.452994, 'Lon': -76.481644});
+    list.push({'Name': "Duffield Hall", 'Lat': 42.445003, 'Lon': -76.482484});
+    list.push({'Name': "Cornell Law School", 'Lat': 42.443851, 'Lon': -76.485520});
+    list.push({'Name': "Insomnia Cookies", 'Lat': 42.441197, 'Lon': -76.485016});
+    list.push({'Name': "Old Tea House", 'Lat': 42.441922, 'Lon': -76.487280});
     res.send(list);
 });
 
